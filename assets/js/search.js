d4f1652feb1d6aa4ade168aaf25600eeeac0e2f2
christiangella <112913805+christiangella@users.noreply.github.com>
2022-10-27 20:27:08 -0700

    psuedocode for search bar

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,6 +1,9 @@
 var searchEl = $('#searchbar')
 
-var urlBeerMapping = 'http://beermapping.com/webservice/locquery/9dac7d6acc6400ddf1e63f04790fb15d/'
+var urlBeerMapping = 'https://beermapping.com/webservice/loccity/9dac7d6acc6400ddf1e63f04790fb15d/davis,ca&s=json'
+
+console.log(urlBeerMapping)
+
 
 // event listener enter ON search bar
 // api search bsed on values put into search bar
@@ -15,3 +18,4 @@ var urlBeerMapping = 'http://beermapping.com/webservice/locquery/9dac7d6acc6400d
 // user can click on a title --> get more data from array and print it to the column beside it, need the following data: name, address, google maps (optional), hours, etc, if they have a description, etc.
 // check in button -- populate brewery name over to the form??? 
 
+// https://beermapping.com/webservice/loccity/9dac7d6acc6400ddf1e63f04790fb15d + value (the search the user inputting into a search bar ) + &s=json
